d0dafaebb246971f842f9163da1e826adff21b1e
Kevin Zhang <zarknight@gmail.com>
2013-05-20 17:15:16 +0800

    update db schema

--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -13,19 +13,35 @@ insert into tbl_user (username, password, email) values ('test2', 'pass2', 'test
 create table tbl_contact (
   id integer not null primary key AUTO_INCREMENT,
   userid integer not null,
-  rolename varchar(128),
-  email varchar(128)
+  firstname varchar(128),
+  lastname varchar(128),
+  email varchar(128),
+  relation varchar(128),
+  create_date timestamp,
+  update_date timestamp
 );
 
 -- Message
-create table tbl_message (
+create table tbl_submission (
   id integer not null primary key AUTO_INCREMENT,
   userid integer not null,
-  rolename varchar(128),
-  email varchar(128)
+  eventid integer,
+  title varchar(128),
+  content text,
+  create_date timestamp,
+  update_date timestamp
 );
 
-create table tbl_message_contact(
+create table tbl_submission_contact (
   contactid integer not null,
   messageid integer not null
+);
+
+create table tbl_event (
+  id integer not null primary key AUTO_INCREMENT,
+  eventname varchar(128),
+  eventdate date,
+  description varchar(256),
+  create_date timestamp,
+  update_date timestamp
 );
