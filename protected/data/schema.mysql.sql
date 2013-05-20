821d094a74c60ec52be3cf01adc4547b43cc11da
Kevin Zhang <zarknight@gmail.com>
2013-05-20 16:48:53 +0800

    add some tables

--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -1,8 +1,31 @@
-CREATE TABLE tbl_user (
-    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(128) NOT NULL,
-    password VARCHAR(128) NOT NULL,
-    email VARCHAR(128) NOT NULL
+-- User
+create table tbl_user (
+  id integer not null primary key AUTO_INCREMENT,
+  username varchar(128) not null,
+  password varchar(128) not null,
+  email varchar(128) not null
 );
 
-INSERT INTO tbl_user (username, password, email) VALUES ('test1', 'pass1', 'test1@example.com');
+insert into tbl_user (username, password, email) values ('test1', 'pass1', 'test1@example.com');
+insert into tbl_user (username, password, email) values ('test2', 'pass2', 'test2@example.com');
+
+-- Contact
+create table tbl_contact (
+  id integer not null primary key AUTO_INCREMENT,
+  userid integer not null,
+  rolename varchar(128),
+  email varchar(128)
+);
+
+-- Message
+create table tbl_message (
+  id integer not null primary key AUTO_INCREMENT,
+  userid integer not null,
+  rolename varchar(128),
+  email varchar(128)
+);
+
+create table tbl_message_contact(
+  contactid integer not null,
+  messageid integer not null
+);
