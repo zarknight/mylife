3d4542b36444c3b21b61c4c00fcff425554f90f2
cherry zhang <cherryzhan@gmail.com>
2013-05-31 15:14:58 +0800

    add user authentication filter

--- protected/controllers/AppController.php
+++ protected/controllers/AppController.php
@@ -4,6 +4,20 @@ class AppController extends Controller
 {
 	public $layout='//layouts/app';
 
+	public function filters() {   
+		return array(   
+			'accessAuth',   
+		);   
+    }   
+
+	public function filterAccessAuth($filterChain) {   
+		if(Yii::app()->user->isGuest)
+			$this->redirect(Yii::app()->homeUrl);
+			
+		$filterChain->run();  
+	}
+
+	
 	public function actionIndex() {
 		$this->render('index');
 	}
